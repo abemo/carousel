d6d2a32d2218db082c0b75820565a710bff5546b
abemo <amooreod@lion.lmu.edu>
2025-10-02 20:57:26 -0700

    added touch screen controls

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ TODOs
 - [x] clean up code, I feel like I have some repeated stuff
 - [x] make carousel size responsive
 - [x] center image should be 50%, left/right 25%
+- [x] add swipe/touch functionality touchstart, touchmove, touchend
 
 - [ ] add nice animated transition
-- [ ] add swipe/touch functionality
 - [ ] write Readme with instructions for running
 - [ ] come up with theme/style
 - [ ] add ability to add and remove images from carousel
+- [ ] add variable swipe (faster/bigger swipe moves more images)
--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-let imageIndex = 0;
+// ====================================================================
+//                          Global Variables
+// ====================================================================
 
 const carouselContent = document.getElementById("carousel-content");
 const images = carouselContent.getElementsByTagName("img");
-
 let totalImages = images.length;
 
 const prevBtn = document.getElementById("prevBtn");
 const playPauseBtn = document.getElementById("playPauseBtn");
 const nextBtn = document.getElementById("nextBtn");
-
 let indicators = document.getElementById("indicators");
 
+let imageIndex = 0;
+let autoRotate = false;
+const autoRotateInterval = 5000;
+const swipeThreshold = 50;
+
+const playSymbol = "&#x23F5;";
+const pauseSymbol = "&#x23F8;";
+const emptyCircleSymbol = "&#9675;";
+const filledCircleSymbol = "&#9679;";
+
+// ====================================================================
+//                          Helper Functions
+// ====================================================================
+
+function wrapIndex(index) {
+  return (index + totalImages) % totalImages;
+}
+
 function incrementImageIndex() {
-  imageIndex = (imageIndex + 1) % totalImages;
+  imageIndex = wrapIndex(imageIndex + 1);
 }
 
 function decrementImageIndex() {
-  imageIndex = (imageIndex - 1 + totalImages) % totalImages;
+  imageIndex = wrapIndex(imageIndex - 1);
 }
+
+// ====================================================================
+//                          Button Controls
+// ====================================================================
 
 prevBtn.addEventListener("click", () => {
   decrementImageIndex();
@@ -29,20 +51,64 @@ nextBtn.addEventListener("click", () => {
   updateCarousel();
 });
 
-let autoRotate = false;
 playPauseBtn.addEventListener("click", () => {
-  if (autoRotate) {
-    playPauseBtn.innerHTML = "&#x23F5;"; // Change to play icon
-  } else {
-    playPauseBtn.innerHTML = "&#x23F8;"; // Change to pause icon
-  }
+  playPauseBtn.innerHTML = autoRotate ? playSymbol : pauseSymbol;
   autoRotate = !autoRotate;
 });
 
-function updateCarousel() {
-  const leftImgIndex = imageIndex - 1 >= 0 ? imageIndex - 1 : totalImages - 1;
-  const centerImgIndex = imageIndex;
-  const rightImgIndex = (imageIndex + 1) % totalImages;
+// ====================================================================
+//                          Touch Screen Controls
+// ====================================================================
+
+let startX = 0;
+let endX = 0;
+
+carouselContent.addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+});
+
+carouselContent.addEventListener("touchmove", (e) => {
+  endX = e.touches[0].clientX;
+});
+
+carouselContent.addEventListener("touchend", () => {
+  const distance = endX - startX;
+
+  if (Math.abs(distance) > swipeThreshold) {
+    if (distance < 0) {
+      incrementImageIndex();
+      updateCarousel();
+    } else {
+      decrementImageIndex();
+      updateCarousel();
+    }
+  }
+  startX = 0;
+  endX = 0;
+});
+
+// ====================================================================
+//                          Carousel Updates
+// ====================================================================
+
+function updateIndicators(imageIndex) {
+  updatedIndicators = Array(totalImages).fill(emptyCircleSymbol);
+  updatedIndicators[imageIndex] = filledCircleSymbol;
+  indicators.innerHTML = updatedIndicators.join("");
+}
+
+function animateTransition(direction) {
+  if (direction === "left") {
+    // TODO
+  } else if (direction === "right") {
+    // TODO
+  }
+}
+
+function updateCarousel(direction) {
+  const leftImgIndex = wrapIndex(imageIndex - 1);
+  const centerImgIndex = wrapIndex(imageIndex);
+  const rightImgIndex = wrapIndex(imageIndex + 1);
 
   Array.from(images).forEach((element) => {
     element.parentElement.classList.remove("left", "center", "right");
@@ -52,10 +118,14 @@ function updateCarousel() {
   images[centerImgIndex].parentElement.classList.add("center");
   images[rightImgIndex].parentElement.classList.add("right");
 
-  updatedIndicators = Array(totalImages).fill("&#9675;");
-  updatedIndicators[imageIndex] = "&#9679;";
-  indicators.innerHTML = updatedIndicators.join("");
+  animateTransition(direction);
+
+  updateIndicators(imageIndex);
 }
+
+// ====================================================================
+//                          Auto Rotate
+// ====================================================================
 
 function applyAutoRotate() {
   if (autoRotate) {
@@ -64,7 +134,6 @@ function applyAutoRotate() {
   }
 }
 
-const autoRotateInterval = 5000;
 setInterval(applyAutoRotate, autoRotateInterval);
 
 updateCarousel();
